e1f5da585bbce19bfe2bdde2c6cd5b4b73a9c0d9
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-08-30 14:30:21 -0400

    Lab 1: Speeding1

---
+++ unit-1/src/main/java/Speeding1.java
@@ -0,0 +1,9 @@
+public class Speeding1 {
+
+    public static int caughtSpeeding(int speed, boolean isBirthday) {
+        if (isBirthday && speed > 85 || !isBirthday && speed > 80) return 2;
+        if (isBirthday && speed > 65 || !isBirthday && speed > 60) return 1;
+        return 0;
+    }
+
+}
