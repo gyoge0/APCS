f8c03ee7997eabf63329e86689e404e8764e7ba1
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-09-07 20:06:28 -0400

    Math assignment: Fibonacci

---
+++ unit-1/src/main/java/com/gyoge/apcs/Fibonacci.java
@@ -0,0 +1,34 @@
+package com.gyoge.apcs;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    private static final HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public static int fib(int n) {
+        if (memo.containsKey(n)) return memo.get(n);
+        int res;
+
+        if (n == 1) res = 0;
+        else if (n == 2) res = 1;
+        else res = fib(n - 1) + fib(n - 2);
+
+        memo.put(n, res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("In: ");
+        int numOfFibNumbers = inp.nextInt();
+        inp.close();
+
+        // write your code below
+        for (int i = 1; i <= numOfFibNumbers; i++) {
+            System.out.printf("%d ", fib(i));
+        }
+    }
+
+}
---
+++ unit-1/src/test/kotlin/com/gyoge/apcs/FibonacciTest.kt
@@ -0,0 +1,14 @@
+package com.gyoge.apcs
+
+import org.junit.jupiter.api.Test
+
+class FibonacciTest : IOTest() {
+    @Test fun `test first 9 Fibonacci`() {
+        setMockInput("9")
+        Fibonacci.main(arrayOf())
+
+        assertOutput(
+            "In: 0 1 1 2 3 5 8 13 21",
+        )
+    }
+}
