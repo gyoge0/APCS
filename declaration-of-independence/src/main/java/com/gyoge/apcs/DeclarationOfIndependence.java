edfc48bc2201737a29a0ba9e86c0f587f59959f4
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-02-08 10:33:43 -0500

    Declaration of independence lab

---
+++ declaration-of-independence/src/main/java/com/gyoge/apcs/DeclarationOfIndependence.java
@@ -0,0 +1,21 @@
+package com.gyoge.apcs;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("unused")
+public class DeclarationOfIndependence {
+    public List<WordFrequency> getWordFrequencies(String text) {
+        return Arrays.stream(text.split(" "))
+                .map(String::toLowerCase)
+                .map(word -> word.replaceAll("[^a-zA-Z]", ""))
+                .filter(word -> !word.isEmpty())
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet().stream()
+                .map(entry -> new WordFrequency(entry.getKey(), entry.getValue().intValue()))
+                .sorted(Comparator.reverseOrder())
+                .toList();
+    }
+}
---
+++ declaration-of-independence/src/main/java/com/gyoge/apcs/WordFrequency.java
@@ -0,0 +1,43 @@
+package com.gyoge.apcs;
+
+@SuppressWarnings("unused")
+public class WordFrequency implements Comparable<WordFrequency> {
+    private String word;
+    private int frequency;
+
+    @Override
+    public String toString() {
+        return "WordFrequency{" +
+                "word='" + word + '\'' +
+                ", frequency=" + frequency +
+                '}';
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public WordFrequency(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public int compareTo(WordFrequency o) {
+        if (this.frequency > o.frequency) return 1;
+        if (this.frequency < o.frequency) return -1;
+        return this.word.compareTo(o.word);
+    }
+}
